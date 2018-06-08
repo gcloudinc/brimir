738b775fd0d52f336a38ce0ec5e78202a76de66b
Yuki SHIDA <shida@be-agile.jp>
2018-06-08 17:03:38 +0900

    * アクセス遮断がうまくいっていなかったのを修正 * 遮断時ステータスを 403 から 404 に変更

--- docker/root/setup_nginx_conf.sh
+++ docker/root/setup_nginx_conf.sh
@@ -11,8 +11,7 @@ fi
 if test -n "$RAILS_RELATIVE_URL_ROOT" && test $RAILS_ENV = "production"; then
   mkdir -p $RAILS_ROOT/public/$RAILS_RELATIVE_URL_ROOT
   ln -sf $RAILS_ROOT/public/assets $RAILS_ROOT/public/$RAILS_RELATIVE_URL_ROOT/assets
-   $APP_ROOT/../.rbenv/versions/*/bin/ruby -pi -e '$_.sub! "__%RAILS_RELATIVE_URL_ROOT%__", "'$RAILS_RELATIVE_URL_ROOT'"' /etc/nginx/sites-available/default
 else
   patch -F 3  -d / -p 0 < /root/remove_nginx_subdirectory_assets.patch 
 fi
-
+$APP_ROOT/../.rbenv/versions/*/bin/ruby -pi -e '$_.sub! "__%RAILS_RELATIVE_URL_ROOT%__", "'$RAILS_RELATIVE_URL_ROOT'"' /etc/nginx/sites-available/default
