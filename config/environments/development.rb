2dc7294983741eca825e6a406b0405640bd2a5fa
Kazuyoshi Tsuya <jetglass@gmail.com>
2018-11-21 17:18:07 +0900

    S3にプライベート属性でファイルをアップロードするようにし、ダウンロードも10分の期限をつけるようにした

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,7 +64,8 @@ Rails.application.configure do
       :s3_credentials => {
         access_key_id: ENV['S3_ACCESS_KEY_ID'],
         secret_access_key: ENV['S3_SECRET_ACCESS_KEY']
-      }
+      },
+      :s3_permissions => :private
     }
   end
 end
